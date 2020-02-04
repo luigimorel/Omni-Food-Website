b136c7bda4d50ee0647e9d6786003a68537eef07
morelmiles <morelmiles@gmail.com>
2020-02-04 17:03:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A company website for an imaginary food delivery business.
+
+![Screenshot](/img/onifood.png)
---
+++ resources/js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+  /* for the sticky navigation*/
+
+  $(".js--section-features").waypoint(
+    function(direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "60px"
+    }
+  );
+  /*scroll on buttons*/
+
+  $(".js--scroll-to-plans").click(function() {
+    $("html,body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
+      1000
+    );
+  });
+
+  $(".js--scroll-to-start").click(function() {
+    $("html,body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
+      1000
+    );
+  });
+  /* Animations on scroll*/
+  $(".js--wp-1").waypoint(
+    function(direction) {
+      $(".js--wp-1").addClass("animated  fadeIn");
+    },
+    {
+      offset: "50%"
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function(direction) {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%"
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function(direction) {
+      $(".js--wp-3").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%"
+    }
+  );
+
+  $(".js--wp-4").waypoint(
+    function(direction) {
+      $(".js--wp-4").addClass("animated pulse");
+    },
+    {
+      offset: "50%"
+    }
+  );
+
+  /*Mobile nav*/
+
+  $(".js--nav-icon").click(function() {
+    var nav = $(".js--main-nav");
+    var icon = $(".js--nav-icon i");
+    nav.slideToggle(200);
+    if (icon.hasClass("ion-navicon-round")) {
+      icon.addClass("ion-close-round");
+      icon.removeClass("ion-navicon-round");
+    } else {
+      icon.addClass("ion-navicon-round");
+      icon.removeClass("ion-close-round");
+    }
+  });
+});
